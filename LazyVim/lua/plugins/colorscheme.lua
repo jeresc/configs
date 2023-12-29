3dac705ea474c085cc612c60b48485b1efa87e5a
jeresc <jeremiastomassrc@gmail.com>
2023-12-28 22:10:13 -0300

    feat: add colorschemes

--- LazyVim/lua/plugins/colorscheme.lua
+++ LazyVim/lua/plugins/colorscheme.lua
@@ -1,23 +1,23 @@
 return {
   { "Shatur/neovim-ayu" },
-  -- { "ellisonleao/gruvbox.nvim" },
-  -- { "catppuccin/nvim", as = "catppuccin" },
-  -- { "sainnhe/gruvbox-material" },
-  -- { "folke/tokyonight.nvim" },
-  -- {
-  -- "navarasu/onedark.nvim",
-  -- opts = {
-  --   style = "darker",
-  -- },
-  -- config = function(_, opts)
-  --   require("onedark").setup(opts)
-  -- end,
-  -- },
+  { "ellisonleao/gruvbox.nvim" },
+  { "catppuccin/nvim", as = "catppuccin" },
+  { "sainnhe/gruvbox-material" },
+  { "folke/tokyonight.nvim" },
+  {
+    "navarasu/onedark.nvim",
+    opts = {
+      style = "darker",
+    },
+    config = function(_, opts)
+      require("onedark").setup(opts)
+    end,
+  },
   -- { "olimorris/onedarkpro.nvim" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "ayu-dark",
+      colorscheme = "ayu",
     },
   },
 }
